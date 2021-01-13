068d2fbeb15080ed73a29064799c22f5d906fff4
Rob Cameron <rob.cameron@fastmail.com>
2021-01-13 10:50:41 -0800

    First pass at secure services

--- packages/api/src/makeServices.ts
+++ packages/api/src/makeServices.ts
@@ -5,5 +5,28 @@ import { MakeServices } from './types'
  * services.
  */
 export const makeServices: MakeServices = ({ services }) => {
-  return services
+  const secureServices: any = {}
+
+  for (const [serviceName, service] of Object.entries(services)) {
+    secureServices[serviceName] = {}
+
+    for (const [funcName, func] of Object.entries(service)) {
+      if (funcName !== 'before') {
+        secureServices[serviceName][funcName] = (...args: Array<unknown>) => {
+          if (service.before) {
+            const beforeResult = service.before({ serviceName: funcName })
+            if (beforeResult || beforeResult === undefined) {
+              return func(...args)
+            } else {
+              throw new Error('Service aborted')
+            }
+          } else {
+            return func(...args)
+          }
+        }
+      }
+    }
+  }
+
+  return secureServices
 }
